a45b18347b35f6a2ed00f0b8442b204dc5e4170d
carolinavlqz <cruzvelazquezcarolina123@gmail.com>
2026-07-15 12:36:22 -0600

    Add files via upload

---
+++ tablet-app/app/ex-empleado-exito.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ExEmpleadoExitoScreen from '../src/screen/ExEmpleadoExitoScreen';
+
+export default function ExEmpleadoExitoRoute() {
+  return <ExEmpleadoExitoScreen />;
+}

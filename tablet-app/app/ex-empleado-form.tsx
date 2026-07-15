fbb7ec2d09b16a235241debdaaf00141c8cedf50
carolinavlqz <cruzvelazquezcarolina123@gmail.com>
2026-07-15 11:56:46 -0600

    Add files via upload

---
+++ tablet-app/app/ex-empleado-form.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// Asegúrate de que la ruta de importación apunte exactamente a donde guardaste el archivo del Paso 3 anterior
+import ExEmpleadoFormScreen from '../src/screen/ExEmpleadoFormScreen'; 
+
+export default function ExEmpleadoFormRoute() { 
+  return <ExEmpleadoFormScreen />; 
+}
